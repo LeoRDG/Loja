0c575bc16d7b7dda96800306fd48ce73150d18f6
Leonardo <leonardo8571@gmail.com>
2025-06-26 18:37:15 -0300

    s

--- js/script.js
+++ js/script.js
@@ -10,8 +10,6 @@ fetch("https://xle00.github.io/Loja/data.json").then(response => response.json()
         produtos.push(produto)
     })
 
-
-
     criarCategorias()
     criarProdutos()
 })
@@ -25,7 +23,7 @@ function getCategorias(){
     const categorias = new Set()
     produtos.forEach(item => {
         item.categoria.replace()
-        categorias.add(item.categoria.replace(/ > .+/, "")) // Filtra sub-categorias
+        categorias.add(item.categoria.replace(/ > .+/, "")) // Filtra sub-categorias com regex
     });
 
     return categorias
@@ -44,7 +42,7 @@ function criarCategorias(){
         a.classList.add("active")
 
         a.innerHTML = cat
-        a.href = "/"
+        a.href = "/" + cat
 
         li.appendChild(a)
         menu.appendChild(li)
@@ -56,30 +54,46 @@ function criarProdutos(){
 
     produtos.forEach(produto => {
         // Criando os elementos
-        const div = document.createElement('div')  // container
-        const img = document.createElement('img')  // imagem
-        const h4 = document.createElement('h4')    // nome
-        const p1 = document.createElement('p')     // preço original
-        const del = document.createElement('del')  // preço original 
-        const p2 = document.createElement('p')     // preço desconto
-        const a = document.createElement('a')      // botão
-
-        // Valor dos elementos
-        div.classList.add("produto")
-        p2.classList.add("preco")
+        const div = document.createElement('div')      // container
+        const img_div = document.createElement('div')  // container para a imagem
+        const pd_div = document.createElement('div')   // container para a preco/desconto
+        const img = document.createElement('img')      // imagem
+        const h4 = document.createElement('h4')        // nome
+        const p1 = document.createElement('p')         // preço original
+        const del = document.createElement('del')      // preço original 
+        const p2 = document.createElement('p')         // preço desconto
+        const p3 = document.createElement('p')         // desconto
+        const a = document.createElement('a')          // botão
         
-        img.src = PLACEHOLDERIMG
-        img.src = `img/${produto.id}.png`
-        h4.innerHTML = produto.nome
-        p1.innerHTML = produto.preco + " R$"
-        p2.innerHTML = produto.precoDesconto.toFixed(2) + " R$"
-        a.innerHTML = "Comprar"
-        del.appendChild(p1)
 
+        // Classes        
+        div.classList.add("produto", "border", "m-2", "p-2", "rounded-4",)
+
+        img_div.classList.add("img-div", "text-center")
+        img.classList.add("img-fluid", "w-75")
+
+        p1.classList.add("text-secondary", "d-inline", "me-3")
+        p3.classList.add("text-danger", "d-inline", "fw-bold")
+
+        p2.classList.add("preco", "text-primary", "fw-bold", "fs-2")
+        a.classList.add("btn", "btn-primary", "p-3", "container-fluid", "rounded-4", "fw-bold")
+        
+        // Valor dos elementos
+        img.src = `img/produtos/${produto.id}.png`
+        h4.innerHTML = produto.nome
+        p1.innerHTML = "R$ " + Intl.NumberFormat("pt-BR", {minimumFractionDigits: 2}).format(produto.preco)
+        p2.innerHTML = "R$ " + Intl.NumberFormat("pt-BR", {minimumFractionDigits: 2, maximumFractionDigits: 2}).format(produto.precoDesconto)
+        p3.innerHTML = produto.promocao * 100 + "% OFF"
+        a.innerHTML = "Comprar"
+        
+        del.appendChild(p1)
+        img_div.appendChild(img)
+        pd_div.append(del, p3)
+        
         // Adicionando os elementos a div
-        div.appendChild(img)
+        div.appendChild(img_div)
         div.appendChild(h4)
-        div.appendChild(del)
+        div.appendChild(pd_div)
         div.appendChild(p2)
         div.appendChild(a)
 
